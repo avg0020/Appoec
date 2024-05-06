6b9e1d39f8a2526e07a2485ba39e73b6dfbce930
pejor76 <pejorcr@gmail.com>
2024-05-06 21:02:05 +0200

    login añadido al manifest
    se esta intentando añadir la funcionalidad

---
+++ TFG/app/src/main/java/com/example/tfg/Datos.java
@@ -0,0 +1,32 @@
+package com.example.tfg;
+
+import java.util.ArrayList;
+
+public class Datos {
+    ArrayList<UserModel> datos=new ArrayList<>();
+    public Datos() {
+
+        //datos.add(new UserModel("PEPE", "ORTIZ"));
+        /*datos.add(new UserModel("JUAN", "PEREZ"));
+        datos.add(new UserModel("ANTONIO", "APELLIDO"));
+        datos.add(new UserModel("ELENA", "OTROAPELLIDO"));
+        datos.add(new UserModel("SILVIA", "APELLIDO4"));
+        datos.add(new UserModel("PEDRO", "APELLIDO5"));
+        datos.add(new UserModel("PEDRO", "APELLIDO5"));
+        datos.add(new UserModel("GUILLERMO", "APELLIDO7"));
+        datos.add(new UserModel("PEDRO", "APELLIDO5"));
+        datos.add(new UserModel("GONZALO", "APELLIDO8"));
+        datos.add(new UserModel("GUILLERMO", "APELLIDO7"));
+        datos.add(new UserModel("PEDRO", "APELLIDO5"));
+        datos.add(new UserModel("GONZALO", "APELLIDO8"));
+        datos.add(new UserModel("FRAN", "APELLIDO9"));
+        datos.add(new UserModel("PEDRO", "APELLIDO5"));
+        datos.add(new UserModel("GUILLERMO", "APELLIDO7"));
+        datos.add(new UserModel("FRAN", "APELLIDO9"));
+        datos.add(new UserModel("GUILLERMO", "APELLIDO7"));*/
+    }
+
+    public ArrayList<UserModel> getDatos() {
+        return datos;
+    }
+}
