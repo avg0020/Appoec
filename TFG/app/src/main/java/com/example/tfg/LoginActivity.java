a79fce5d11409807518e989b6abb44cd1c92adb9
pejor76 <pejorcr@gmail.com>
2024-05-11 19:32:55 +0200

    Arreglos

--- TFG/app/src/main/java/com/example/tfg/LoginActivity.java
+++ TFG/app/src/main/java/com/example/tfg/LoginActivity.java
@@ -58,7 +58,7 @@ public class LoginActivity extends AppCompatActivity {
                         if(snapshot.hasChild(nom) && !nom.equals("")) {
                             Log.d("k","que haces?");
                             if(snapshot.child(nom).child("password").getValue(String.class).equals(pass)) {
-                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                startActivity(new Intent(LoginActivity.this, Prueba.class));
                             }
                         }
                     }
