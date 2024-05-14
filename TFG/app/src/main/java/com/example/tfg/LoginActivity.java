8a18fb7d3a44daa693cf8f7024822457075b0b93
avg0020 <avg0020@alu.medac.es>
2024-05-14 21:08:49 +0200

    basic config

--- TFG/app/src/main/java/com/example/tfg/LoginActivity.java
+++ TFG/app/src/main/java/com/example/tfg/LoginActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -51,15 +52,17 @@ public class LoginActivity extends AppCompatActivity {
                 database.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        Log.d("E","Entro");
                         String nom = editTextNom.getText().toString()+"";
                         String pass = editTextPass.getText().toString()+"";
-                        Log.d("E",""+ snapshot.hasChild(nom));
-                        if(snapshot.hasChild(nom) && !nom.equals("")) {
-                            Log.d("k","que haces?");
-                            if(snapshot.child(nom).child("password").getValue(String.class).equals(pass)) {
+                        Toast.makeText(getApplicationContext(), String.valueOf(snapshot.hasChild(nom)), Toast.LENGTH_LONG).show();
+                        if (!nom.isEmpty() && !pass.isEmpty()){
+                            if(snapshot.hasChild(nom) && snapshot.child(nom).child("password").getValue(String.class).equals(pass)) {
                                 startActivity(new Intent(LoginActivity.this, Interfaz.class));
+                            }else{
+                                Toast.makeText(getApplicationContext(), "usuario o contraseña incorrecto", Toast.LENGTH_LONG).show();
                             }
+                        }else {
+                            Toast.makeText(getApplicationContext(), "Debe rellenar todos los campos", Toast.LENGTH_LONG).show();
                         }
                     }
 
