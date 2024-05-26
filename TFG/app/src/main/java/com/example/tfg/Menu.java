6a5a57409aec685b206d8aa7c18272ca416da16b
juaaaanmedac <jpl0010@alu.medac.es>
2024-05-26 14:12:44 +0200

    login para usuarioss y empleados

--- TFG/app/src/main/java/com/example/tfg/Menu.java
+++ TFG/app/src/main/java/com/example/tfg/Menu.java
@@ -1,8 +1,6 @@
 package com.example.tfg;
 
 import android.os.Bundle;
-
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -16,12 +14,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,12 +31,9 @@ import java.util.ArrayList;
  */
 public class Menu extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -50,15 +48,6 @@ public class Menu extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment menu.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Menu newInstance(String param1, String param2) {
         Menu fragment = new Menu();
         Bundle args = new Bundle();
@@ -67,16 +56,6 @@ public class Menu extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
-    /*
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }*/
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -90,57 +69,61 @@ public class Menu extends Fragment {
 
         Bundle args = getArguments();
         Usuarios user = (Usuarios) args.getSerializable("user");
-        // Inflate the layout for this fragment
-        //setSupportActionBar(getView().findViewById(R.id.toolbar));
+        if (user == null) {
+            Log.e("Menu", "El usuario es nulo.");
+            Toast.makeText(getContext(), "Error: Usuario no disponible.", Toast.LENGTH_LONG).show();
+            return v;
+        }
 
         RecyclerView recycleViewUser = (RecyclerView) v.findViewById(R.id.recycleViewUser);
-        // use a linear layout manager (distribucion de vistas configurable)
-        //como queremos que se posicionen los elementos en las vistas, como lista o como cuadricula GridLayout
         recycleViewUser.setLayoutManager(new LinearLayoutManager(getContext()));
         recycleViewUser.setHasFixedSize(false);
-        //puedo añadir animaciones automaticas (ItemAnimator) y sepaaciones automaticas (ItemDecoration)
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference().child("actividades");
 
-        // It is a class provide by the FirebaseUI to make a
-        // query in the database to fetch appropriate data
-       /* FirebaseRecyclerOptions<Actividades> options
-                = new FirebaseRecyclerOptions.Builder<Actividades>()
-                .setQuery(myRef.orderByKey().equalTo("BAI04A"), Actividades.class).build();
-        Log.d("child", myRef.child("BAI").getKey());
-        adapter = new ChildrenActivityAdapter(options,getContext(),user);
-        */
         ConcatAdapter adapters = new ConcatAdapter();
 
-        for (String actividad:user.getHijos().get("fulanita").getActividades()) {
-            FirebaseRecyclerOptions<Actividades> options
-                    = new FirebaseRecyclerOptions.Builder<Actividades>()
-                    .setQuery(myRef.orderByKey().equalTo(actividad), Actividades.class).build();
-            adapter = new ChildrenActivityAdapter(options,getContext(),user);
-            adapter.startListening();
-            adapters.addAdapter(adapter);
+        Map<String, Alumno> hijos = user.getHijos();
+        if (hijos != null) {
+            Alumno alumno = hijos.get("fulanita");
+            if (alumno != null) {
+                List<String> actividades = alumno.getActividades();
+                if (actividades != null) {
+                    for (String actividad : actividades) {
+                        FirebaseRecyclerOptions<Actividades> options = new FirebaseRecyclerOptions.Builder<Actividades>()
+                                .setQuery(myRef.orderByKey().equalTo(actividad), Actividades.class)
+                                .build();
+                        adapter = new ChildrenActivityAdapter(options, getContext(), user);
+                        adapter.startListening();
+                        adapters.addAdapter(adapter);
+                    }
+                } else {
+                    Log.e("Menu", "El alumno 'fulanita' no tiene actividades.");
+                    Toast.makeText(getContext(), "El alumno 'fulanita' no tiene actividades.", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Log.e("Menu", "El alumno 'fulanita' no existe.");
+                Toast.makeText(getContext(), "El alumno 'fulanita' no existe.", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Log.e("Menu", "El usuario no tiene hijos.");
+            Toast.makeText(getContext(), "El usuario no tiene hijos.", Toast.LENGTH_LONG).show();
         }
 
-        // specify an adapter with the list to show
         recycleViewUser.setAdapter(adapters);
-
         return v;
     }
 
-    @Override public void onStart()
-    {
+    @Override public void onStart() {
         super.onStart();
-        Log.d("dasdsadas","entrado");
-
+        Log.d("Menu", "Fragmento iniciado.");
     }
 
-    // Function to tell the app to stop getting
-    // data from database on stopping of the activity
-    @Override public void onStop()
-    {
+    @Override public void onStop() {
         super.onStop();
-        adapter.stopListening();
+        if (adapter != null) {
+            adapter.stopListening();
+        }
     }
-
 }
