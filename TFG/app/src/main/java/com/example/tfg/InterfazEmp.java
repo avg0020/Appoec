2fd8d38a9a249d1d4afd2a716403867b01da46a3
GuillermoDaniGarcia <163834885+GuillermoDaniGarcia@users.noreply.github.com>
2024-06-03 20:23:23 +0200

    Empleados. Falta calendar

---
+++ TFG/app/src/main/java/com/example/tfg/InterfazEmp.java
@@ -0,0 +1,189 @@
+package com.example.tfg;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class InterfazEmp extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private FrameLayout fragmentContainer;
+    private TextView option1, option2, option3, option4, option5, option6, option7;
+    private LinearLayout menuLayoutComedor, menuLayout;
+    private DrawerLayout drawerLayout;
+    private Usuarios user;
+    private String nom;
+
+    ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interfaz_empleado);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(findViewById(R.id.toolbar));
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, 0, 0);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navView = findViewById(R.id.navigation_view);
+        navView.setNavigationItemSelectedListener(this);
+        navView.setCheckedItem(R.id.nav_icon1);
+
+        user = (Usuarios) getIntent().getSerializableExtra("Usuarios");
+        nom=  getIntent().getStringExtra("nombre");
+        // Referencias de los elementos del layout
+        fragmentContainer = findViewById(R.id.fragment_container);
+
+        option1 = findViewById(R.id.option1);
+        option2 = findViewById(R.id.option2);
+        option3 = findViewById(R.id.option3);
+        option4 = findViewById(R.id.option4);
+        option5 = findViewById(R.id.option5);
+        option6 = findViewById(R.id.option6);
+        option7 = findViewById(R.id.option7);
+        menuLayoutComedor = findViewById(R.id.menuLayoutComedor);
+        menuLayout = findViewById(R.id.menuLayout);
+
+        MenuEmpleado menuFragment = new MenuEmpleado();
+        Bundle args = new Bundle();
+        args.putSerializable("user", user);
+        args.putString("nombre", nom);
+        menuFragment.setArguments(args);
+        changeFragment(menuFragment);
+
+
+
+        // Manejar clics en las opciones del menú
+        option1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MenuEmpleado menuFragment = new MenuEmpleado();
+                Bundle args = new Bundle();
+                args.putSerializable("user", user);
+                args.putString("nombre", nom);
+                menuFragment.setArguments(args);
+                changeFragment(menuFragment);
+            }
+        });
+
+        option2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+           Calendar caleFragment=new Calendar();
+                Bundle args = new Bundle();
+                args.putSerializable("user", user);
+                args.putString("nombre", nom);
+                caleFragment.setArguments(args);
+                changeFragment(caleFragment);
+            }
+        });
+
+        option3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MessageEmp messageFragment = new MessageEmp();
+                Bundle args = new Bundle();
+                args.putSerializable("user", user);
+                args.putString("nombre", nom);
+                messageFragment.setArguments(args);
+                changeFragment(messageFragment);
+            }
+        });
+
+        option4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllActivities allActivities = new AllActivities();
+                Bundle args = new Bundle();
+                args.putSerializable("user", user);
+                args.putString("nombre", nom);
+                allActivities.setArguments(args);
+                changeFragment(allActivities);
+            }
+        });
+
+        option5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InicioComedor inicioComedor = new InicioComedor();
+                Bundle args = new Bundle();
+                args.putSerializable("user", user);
+                inicioComedor.setArguments(args);
+                changeFragment(inicioComedor);
+            }
+        });
+
+        option6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MessageComedor messageComedor = new MessageComedor();
+                Bundle args = new Bundle();
+                args.putSerializable("user", user);
+                messageComedor.setArguments(args);
+                changeFragment(messageComedor);
+            }
+        });
+
+        option7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RecomendacionesComedor recomendacionesComedor = new RecomendacionesComedor();
+                Bundle args = new Bundle();
+                args.putSerializable("user", user);
+                recomendacionesComedor.setArguments(args);
+                changeFragment(recomendacionesComedor);
+            }
+        });
+    }
+
+    private void changeFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Toast.makeText(this, "Hola", Toast.LENGTH_SHORT).show();
+        if(menuItem.getItemId() == R.id.nav_icon1) {
+            if (menuLayout.getVisibility() == View.INVISIBLE){
+                Menu menuFragment = new Menu();
+                Bundle args = new Bundle();
+                args.putSerializable("user", user);
+                menuFragment.setArguments(args);
+                changeFragment(menuFragment);
+            }
+            menuLayout.setVisibility(View.VISIBLE);
+            menuLayoutComedor.setVisibility(View.INVISIBLE);
+        } else if (menuItem.getItemId() == R.id.nav_icon2) {
+            if (menuLayoutComedor.getVisibility() == View.INVISIBLE){
+                InicioComedor inicioComedor = new InicioComedor();
+                Bundle args = new Bundle();
+                args.putSerializable("user", user);
+                inicioComedor.setArguments(args);
+                changeFragment(inicioComedor);
+            }
+            menuLayout.setVisibility(View.INVISIBLE);
+            menuLayoutComedor.setVisibility(View.VISIBLE);
+        }
+        return true;
+    }
+}
---
+++ TFG/app/src/main/java/com/example/tfg/MenuEmpleado.java
@@ -0,0 +1,115 @@
+package com.example.tfg;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ConcatAdapter;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MenuEmpleado#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MenuEmpleado extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    ChildrenActivityAdapter adapter;
+
+    public MenuEmpleado() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MenuEmpleado.
+     */
+    public static MenuEmpleado newInstance(String param1, String param2) {
+        MenuEmpleado fragment = new MenuEmpleado();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_menu_empleado, container, false);
+
+        Bundle args = getArguments();
+        Usuarios user = (Usuarios) args.getSerializable("user");
+
+        RecyclerView recycleViewUser = (RecyclerView) v.findViewById(R.id.recycleViewUser);
+        recycleViewUser.setLayoutManager(new LinearLayoutManager(getContext()));
+        recycleViewUser.setHasFixedSize(false);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference().child("actividades");
+
+        loadRecycler(user, myRef, recycleViewUser);
+
+        return v;
+    }
+
+    private void loadRecycler(Usuarios user, DatabaseReference myRef, RecyclerView recycleViewUser) {
+        ConcatAdapter adapters = new ConcatAdapter();
+        List<String> actividades = user.getActividades();
+
+        for (String actividad : actividades) {
+            FirebaseRecyclerOptions<Actividades> options = new FirebaseRecyclerOptions.Builder<Actividades>()
+                    .setQuery(myRef.orderByKey().equalTo(actividad), Actividades.class).build();
+            adapter = new ChildrenActivityAdapter(options, getContext(), user);
+            adapter.startListening();
+            adapters.addAdapter(adapter);
+        }
+
+        recycleViewUser.setAdapter(adapters);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (adapter != null) {
+            adapter.stopListening();
+        }
+    }
+}
